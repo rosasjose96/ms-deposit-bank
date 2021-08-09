a9715c7773509c0184dc216345fbda33aa5bd149
OrlandoKuan <orlandokuanb@gmail.com>
2021-08-08 19:40:35 -0500

    Initial commit and add entities

---
+++ src/main/java/com/example/msdepositbank/MsDepositBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.msdepositbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDepositBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDepositBankApplication.class, args);
+	}
+
+}
